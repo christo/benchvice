41828bfb26db911066429f0fb905364d73f89628
Chris Mountford <chris@think.net.au>
2024-07-28 18:20:45 +1000

    simplified bash version for comparison

--- vmon.sh
+++ vmon.sh
@@ -39,6 +39,7 @@ function dump_mem() {
 function dump_mem_wait() {
   sleep 5
   screenshot "$1.png"
+  vmon "screen " >"$1.scr.txt"
   dump_mem "$1"
   hexdump -vC "$1" > "$1.hex"
   sleep 5
@@ -46,29 +47,27 @@ function dump_mem_wait() {
 
 xvic -remotemonitor -remotemonitoraddress "$monitor_address" \
     -binarymonitor \
+    -config "vice.config" \
     -memory 8k \
     -autostartprgmode 1 \
     "$sargon_prg" \
     >vice.out.log &
 
 
-dump_mem_wait "1pre_load.mem"
 echo waiting for prg to load
 sleep 6
-dump_mem_wait "2post_load.mem"
 echo centering screen
 for ((i=0; i<7; i++)); do
     send_keys "$CHR_CURSOR_RIGHT$CHR_CURSOR_DOWN"
 done
 send_keys "$CHR_CURSOR_DOWN$CHR_CURSOR_DOWN"
-dump_mem_wait "3post_screen_centre.mem"
+dump_mem_wait "1post_screen_centre.mem"
 echo starting game as white on level 0
 send_keys $CHR_F1
-dump_mem_wait "4post_game_f1.mem"
 send_keys "gw0"
-dump_mem_wait "5post_game_select.mem"
+dump_mem_wait "2post_game_select.mem"
 echo entering white move d2-d4
 send_keys "d2-d4$CHR_RETURN"
-dump_mem_wait "6post_move.mem"
-vmon "quit"
+dump_mem_wait "3post_move.mem"
+
 
