d86867b28db7b7607301a7769a1f50a7b8875b0e
Chris Mountford <chris@think.net.au>
2024-08-14 12:42:15 +1000

    wip debugging

--- src/vice_connect.py
+++ src/vice_connect.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
 from abc import ABC, abstractmethod
+import asyncio
 import socket
 import struct
+
 
 # binary monitor interface with partial command implementation
 # see vice manual section 12 https://vice-emu.sourceforge.io/vice_12.html
@@ -65,79 +67,78 @@ ERR_MESG = {
 
 }
 
-# non-threadsafe monotonically increasing sequence number
-request_id = 0
+class ResponseHeader:
+    """Response header fields"""
+
+    def __init__(self, magic, ver, resp_length, response_type, error_code, resp_req_id):
+        super().__init__()
+        self.magic = magic
+        self.ver = ver
+        self.body_len = resp_length
+        self.response_type = response_type
+        self.error_code = error_code
+        self.req_id = resp_req_id
+
 
 class SocketPair(ABC):
 
     @abstractmethod
-    def open(self, host, port):
-        pass
-
-    @abstractmethod
-    def read(self, length):
-        pass
-
-    @abstractmethod
-    def write(self, data):
-        pass
-
-    @abstractmethod
-    def command(self, data):
-        pass
-
-    @abstractmethod
-    def close(self):
+    def command(self, req_id, req):
         pass
 
 
 class AsyncSocketPair(SocketPair):
 
-    def open(self, host, port):
-        pass
-
-    def read(self, length):
-        pass
-
-    def write(self, data):
-        pass
-
-    def command(self, data):
-        pass
-
-    def close(self):
-        pass
-
-    def __init__(self):
+    def __init__(self, host, port):
         super().__init__()
+        self.host = host
+        self.port = port
+
+    def command(self, req_id, req):
+        async def execute():
+            reader, writer = await asyncio.open_connection(self.host, self.port)
+
+            try:
+                print(f'async: sending request')
+                writer.write(req)
+                await writer.drain()
+
+                header_len = RESPONSE_HEADER_LENGTH
+                header = reader.readexactly(header_len)
+                response_header = parse_res_header(header)
+                body = reader.readexactly(response_header.body_len)
+                return body
+
+            finally:
+                print('async: closing the connection')
+                writer.close()
+                await writer.wait_closed()
+
+        asyncio.run(execute())
 
 
 class SyncSocketPair(SocketPair):
 
-    def open(self, host, port):
+    def __init__(self, host, port):
+        super().__init__()
+        self.host = host
+        self.port = port
+
+    def command(self, req_id, req):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            # sock.settimeout(10)
+            sock.connect((self.host, self.port))
+            sock.sendall(req)
+            return receive_response(sock, req_id)
         pass
 
-    def read(self, length):
-        pass
 
-    def write(self, data):
-        pass
-
-    def command(self, data):
-        pass
-
-    def close(self):
-        pass
-
-
-def cmd_memory_get(from_addr, to_addr):
+def cmd_memory_get(request_id, from_addr, to_addr):
 
     # length of request body is fixed for memory get
     # side effects (1), from addr (2), to addr (2), memspace (1), bank_id (2)
     body_length = 1 + 2 + 2 + 1 + 2
 
-    global request_id
-    request_id = request_id + 1
     # format the request packet, everything is little endian
     request = struct.pack(">BBIIBBHHBH",
                           API_START,            # api start: 8 bit
@@ -153,15 +154,14 @@ def cmd_memory_get(from_addr, to_addr):
     return request
 
 
-def cmd_exit():
-    global request_id
-    request_id = request_id + 1
+def cmd_exit(request_id):
     request = struct.pack(">BBIIB",
                           API_START,            # api start: 8 bit
                           API_VERSION,          # api version 8 bit
                           0,                    # body size: 32 bit
                           request_id,           # request id: 32 bit
                           CMD_EXIT)             # command id: 8 bit
+    return request
 
 
 def write_binary_file(data, filename):
@@ -171,13 +171,13 @@ def write_binary_file(data, filename):
 def hex_n_decimal(value):
     return f"{hex(value)} ({value})"
 
-def socket_read(sockt, resp_length):
-    data = sockt.recv(resp_length)
-    while len(data) < resp_length:
-        data += sockt.recv(length - len(data))
+def socket_read_exactly(sockt, n):
+    data = sockt.recv(n)
+    while len(data) < n:
+        data += sockt.recv(n - len(data))
     return data
 
-def get_vice_memory_contents_binary(host: str, port: int, from_addr: int, to_addr: int) -> bytes:
+def get_vice_memory_contents_binary(host: str, port: int, from_addr: int, to_addr: int, req_id: int) -> bytes:
     """
     Connect to the VICE binary monitor and get memory contents of a given address range.
 
@@ -185,43 +185,43 @@ def get_vice_memory_contents_binary(host: str, port: int, from_addr: int, to_add
     :param port: The port number of the VICE monitor.
     :param from_addr: The start address of the memory range.
     :param to_addr: The end address of the memory range.
+    :param req_id: session unique request identifier
     :return: The memory contents as bytes.
     """
 
     if not 0 <= from_addr <= to_addr <= 0xffff:
         raise ValueError("start and finish must be in range 0x0-0xffff and start <= finish")
 
-    # TODO move this implementation to SyncSocketPair.command
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        # sock.settimeout(10)
-        sock.connect((host, port))
-        request = cmd_memory_get(from_addr, to_addr)
-        write_binary_file(request, 'memory_get_req.bin')
-        sock.sendall(request)
-        return receive_response(sock)
+    sp = SyncSocketPair(host, port)
+    get_memory = cmd_memory_get(from_addr, to_addr, req_id)
+    write_binary_file(get_memory, "get_binary_file.req.bin")
+    return sp.command(req_id, get_memory)
 
 
-def receive_response(sockt):
-
+def receive_response(sockt, request_id):
+    """
+    Reads the response from a socket and returns it as a byte array.
+    :param sockt:
+    :param request_id:
+    :return:
+    """
     # Read the response header
     # then we know the body length and we can read the body
 
     response = 1
     body = b''
-    event_response = True   # keep reading until we find a response for a normal request
-    while event_response:
+    found_response = False   # keep reading until we find a response for a normal request
+    while not found_response:
         print(f"\nresponse {response} reading {RESPONSE_HEADER_LENGTH} header bytes")
-        header = socket_read(sockt, RESPONSE_HEADER_LENGTH)
-        magic, ver, body_len, response_type, error_code, resp_req_id = parse_res_header(header)
-        event_response = resp_req_id == REQ_ID_EVENT
-        if body_len > 0:
-            # reading body bytes but it's not the droids we're looking for
-            body = socket_read(sockt, body_len)
+        header = socket_read_exactly(sockt, RESPONSE_HEADER_LENGTH)
+        response_header = parse_res_header(header)
+        found_response = resp_req_id != REQ_ID_EVENT
+        if response_header.body_len > 0:
+            body = socket_read_exactly(sockt, response_header.body_len)
             print(f" body: {hex_dump(body)}")
-        if not event_response and resp_req_id != request_id:
+        if found_response and response_header.req_id != request_id:
             raise ValueError(f"got response for request {resp_req_id} expected {request_id}")
         response = response + 1
-
     # response body format for get memory:
     # length of memory segment: 2 bytes
     # value at memory address from_addr + n for each n: n+1 bytes
@@ -250,14 +250,14 @@ def parse_res_header(header):
         raise ValueError("scuffed response packet")
     elif error_code != ERR_NONE:
         raise ValueError(ERR_MESG[error_code])
-    return magic, ver, resp_length, response_type, error_code, resp_req_id
+    return Response(magic, ver, resp_length, response_type, error_code, resp_req_id)
 
 
 def hex_dump(bs):
     return ' '.join(f'{b:02x}' for b in bs)
 
 def main():
-    resp = get_vice_memory_contents_binary("127.0.0.1", 6502, 15, 16)
+    resp = get_vice_memory_contents_binary("127.0.0.1", 6502, 15, 16, 1)
     print(resp)
 
 
--- src/vmon.sh
+++ src/vmon.sh
@@ -3,17 +3,18 @@
 # starts sargon ii on vice vic 20 emulator
 # depends on nc, xvic (from vice)
 
-source source.sh
+SCRIPT_DIR=$(dirname "$0")
+source "$SCRIPT_DIR/source.sh"
 
 xvic -remotemonitor -remotemonitoraddress "$monitor_address" \
-    -binarymonitor \
-    -config "vice.config" \
-    -memory 8k \
-    -autostartprgmode 1 \
-    "$sargon_prg" \
-    >vice.out.log &
+      -binarymonitor \
+      -config "$SCRIPT_DIR/../vice.config" \
+      -memory 8k \
+      -autostartprgmode 1 \
+      "$SCRIPT_DIR/../$sargon_prg" \
+      >"$SCRIPT_DIR/../vice.out.log" &
 
-
+exit
 
 echo waiting for prg to load
 sleep 6
@@ -21,16 +22,19 @@ echo centering screen
 shift_right=$(printf "%0.s$CHR_CURSOR_RIGHT" {1..7})
 shift_down=$(printf "%0.s$CHR_CURSOR_DOWN" {1..10})
 send_keys "${shift_right}${shift_down}"
+sleep 1
 #dump_mem_wait "1post_screen_centre.mem"
 echo starting game as white on level 0
 send_keys $CHR_F1
 send_keys "gw0"
+sleep 1
 #dump_mem_wait "2post_game_select.mem"
 echo entering white move d2-d4
 send_keys "d2-d4$CHR_RETURN"
 #dump_mem_wait "3post_move.mem"
+sleep 1
+send_keys "c1-f4$CHR_RETURN"
 
-
-is_human_turn
+#is_human_turn
 RV=$?
 exit $RV
